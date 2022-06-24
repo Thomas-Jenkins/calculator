95196cd2bb812316299bcdbfcda62ce62735043f
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-06-23 19:22:30 -0700

    add basic functionality of an addition calculator

--- app.js
+++ app.js
@@ -6,3 +6,17 @@
     // logic and calculations
     // update UI
 
+import { add } from '../utils.js';
+
+const addInput1 = document.getElementById('add-input-1');
+const addInput2 = document.getElementById('add-input-2');
+const addButton = document.getElementById('add-button');
+const addResult = document.getElementById('add-result');
+
+console.log(addInput1, addInput2, addButton, addResult)
+
+addButton.addEventListener('click', () => {
+const num1 = Number(addInput1.value);
+const num2 = Number(addInput2.value);
+addResult.textContent = add(num1, num2);
+})
--- test/tests.js
+++ test/tests.js
@@ -2,15 +2,18 @@
 // import { add } from '../utils.js';
 
 const test = QUnit.test;
+import { add } from '../utils.js';
 
 test('test your pure functions...', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 8;
+    const num1 = 3;
+    const num2 = 5;
 
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = add(num1, num2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
